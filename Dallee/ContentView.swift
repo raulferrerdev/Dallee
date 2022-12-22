aef6d34c0d860bb2d3f1a3ceab5c32ff2b907838
Raúl <raul.ferrer.dev@gmail.com>
2022-12-22 06:59:46 +0100

    Initial Commit

---
+++ Dallee/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Dallee
+//
+//  Created by Raúl Ferrer on 22/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dallee/DalleeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DalleeApp.swift
+//  Dallee
+//
+//  Created by Raúl Ferrer on 22/12/22.
+//
+
+import SwiftUI
+
+@main
+struct DalleeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
